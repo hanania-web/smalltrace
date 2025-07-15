9199caeb79d3b73853a128eae2c1f21b92f66501
Hanania <cvhananiaa@gmail.com>
2025-07-15 23:40:10 +0700

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 Object.defineProperty(document, 'referrer', {
-  get: () => 'https://medium.com/',
+  get: () => 
+  'https://www.facebook.com/groups/cryptorewardsdaily/posts/1071283/',
+  'https://cryptobonusdaily.blogspot.com/2025/07/claim-reward-crypto-airdrop-free.html',
+  'https://medium.com/@cryptotipszone/top-crypto-rewards-sites-2025-airdrop-dapps-4df3b97a4cf6',
+  'https://bitcointalk.org/index.php?topic=5482101.0',
+  'https://t.me/s/freeairdropsignal',
+  'https://bitcointalk.org/index.php?topic=5482101.0',
+  'https://cryptotalk.org/topic/43567-top-reward-sites-with-daily-crypto-payouts/',
+  'https://www.altcoinstalks.com/index.php?topic=353622.msg1975443',
+  'https://cryptobonusdaily.blogspot.com/2025/07/claim-reward-crypto-airdrop-free.html',
+  'https://medium.com/@cryptotipszone/top-crypto-rewards-sites-2025-airdrop-dapps-4df3b97a4cf6',
+  'https://coinrewardsjournal.wordpress.com/2025/07/10/how-to-earn-bitcoin-without-investment/',
+  'https://cryptoreward.tumblr.com/post/1234567890/dapatkan-bitcoin-setiap-hari-dengan-website-ini',
+  'https://cryptohustle.xyz/articles/top-5-free-airdrop-sites-no-kyc/',
+  'https://www.facebook.com/groups/cryptorewardsdaily/posts/1071283/',
+  'https://www.reddit.com/r/cryptofreebies/comments/14k39c3/free_crypto_airdrop_today/',
+  'https://twitter.com/cryptoreward_bot/status/1728821836371826688',
+  'https://t.me/s/freeairdropsignal',
+  'https://www.linkedin.com/pulse/5-best-crypto-reward-sites-2025-edition-john-doe/'
 });
